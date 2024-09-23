f155c58d86a5a69775f6859650d87e3b36c1d6e9
DimT <dimtryal1000@mail.ru>
2024-09-23 12:47:51 +0300

    Добавьте файлы проекта.

---
+++ Figures/Figures/Triangle.cs
@@ -0,0 +1,34 @@
+﻿namespace Figures
+{
+    public class Triangle : IGeometricalFigure
+    {
+        public double GetSquare(params double[] numbers)
+        {
+            if (!IsItTriangle(numbers))
+            {
+                // logger.log не треугольник
+                return 0;
+            }
+
+            double halfPerimetr = numbers.Sum() / 2;
+
+            //площать по трем сторонам формула Герона
+            return Math.Sqrt(
+                halfPerimetr *
+                (halfPerimetr - numbers[0]) *
+                (halfPerimetr - numbers[1]) *
+                (halfPerimetr - numbers[3]));
+        }
+
+        private bool IsItTriangle(double[] numbers) 
+        {
+            if (numbers.Length == 3)
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
---
+++ FiguresTests/Figures/TriangleTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void GetSquareTest()
+        {
+            //arrange
+            double x = 3;
+            double y = 4;
+            double z = 5;
+
+            double expectedSquare = 6;
+            //act
+            Triangle triangle = new Triangle();
+            var squere = triangle.GetSquare(x, y, z);
+
+
+            //assert
+            Assert.AreEqual(expectedSquare, squere);
+        }
+    }
+}
